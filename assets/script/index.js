45bf52a8baa5491099e30ef27040eb673a203a4e
Mehriban Ibrahimova <zahidibrahimov@MacBook-Air-Zahid-6.local>
2021-06-18 16:08:08 +0400

    clients-images

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,11 +13,13 @@ buttonF.onclick = function () {
 buttonS.onclick = function () {
   content.innerHTML =
     "In the last decade, smartphones became an absolute necessity for human life. It is the main reason why we build the most innovative apps which help organizations to stay in fashion. Our highly talented and creative team has a capability to develop state-of-the-art mobile applications in various complexity for IOS and Android platforms. We implement advanced features with a delicate user interface and establish a secure data environment.";
+  //   this.style.backgroundColor = " #566cb3";
 };
 
 buttonT.onclick = function () {
   content.innerHTML =
     "Today, business world, to a large extent, run with operative software programs. In Caspian Soft, we are specialized in implementing a wide range of software solutions to help our clients to maximize their productivity, efficiency, and profitability. These sophisticated programs do not just provide convenience but also assure the professionalism and competitiveness in a modern business environment.";
+  changeColor();
 };
 
 var typed = new Typed("#typed", {
@@ -26,3 +28,7 @@ var typed = new Typed("#typed", {
   backDelay: 700,
   loop: true,
 });
+
+function changeColor() {
+  buttonF.style.backgroundColor = "$blue-color";
+}
