47c88ec94de3ef591d148e049867af9e74242bcd
Mehriban Ibrahimova <zahidibrahimov@MacBook-Air-Zahid-6.local>
2021-06-18 11:05:39 +0400

    nazrin's last commit

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,16 +7,17 @@ var content = document.querySelector(".modelpr");
 
 buttonF.onclick = function () {
   content.innerHTML =
-    "Nunc ac purus non est porttitor maximus sollicitudin in mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sit amet scelerisque urna. Fusce eget erat mauris. Cras viverra tempus ipsum. Duis rutrum arcu ut tempor gravida. Ut vel arcu ac velit commodo tempor sed nec lacus. Aenean pulvinar lectus tempus quam accumsan vehicula.";
+    "Ever since the Internet has been introduced, web services skyrocketed. With a growing demand for e-commerce facilities, Caspian Soft offers a wide range of reliable solutions for organizations. Our main competencies in web development lie in designing modern corporate websites; building e-commerce platforms with high usability and sleek aesthetic, and tailoring online tools for the very niche requirements of the clients.";
 };
 
 buttonS.onclick = function () {
-  content.innerHTML = "Yoxlayaq gorek";
+  content.innerHTML =
+    "In the last decade, smartphones became an absolute necessity for human life. It is the main reason why we build the most innovative apps which help organizations to stay in fashion. Our highly talented and creative team has a capability to develop state-of-the-art mobile applications in various complexity for IOS and Android platforms. We implement advanced features with a delicate user interface and establish a secure data environment.";
 };
 
 buttonT.onclick = function () {
   content.innerHTML =
-    "Nunc ac purus non est porttitor maximus sollicitudin in mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sit amet scelerisque urna. Fusce eget erat mauris. Cras viverra tempus ipsum. Duis rutrum arcu ut tempor gravida. Ut vel arcu ac velit commodo tempor sed nec lacus. Aenean pulvinar lectus tempus quam accumsan vehicula.";
+    "Today, business world, to a large extent, run with operative software programs. In Caspian Soft, we are specialized in implementing a wide range of software solutions to help our clients to maximize their productivity, efficiency, and profitability. These sophisticated programs do not just provide convenience but also assure the professionalism and competitiveness in a modern business environment.";
 };
 
 var typed = new Typed("#typed", {
