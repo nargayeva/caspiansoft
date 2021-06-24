2518171e88ccda7d8a9e9155dcabe101c348877a
Narmina Aghayeva <nargayeva@gmail.com>
2021-06-24 12:00:52 +0400

    Narmin's last changed

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,20 +6,21 @@ var buttonT = document.querySelector(".software-consulting-btn"); //third button
 var content = document.querySelector(".modelparagh");
 
 buttonF.onclick = function () {
+  element.classList.add("chosen");
   content.innerHTML =
     "Ever since the Internet has been introduced, web services skyrocketed. With a growing demand for e-commerce facilities, Caspian Soft offers a wide range of reliable solutions for organizations. Our main competencies in web development lie in designing modern corporate websites; building e-commerce platforms with high usability and sleek aesthetic, and tailoring online tools for the very niche requirements of the clients.";
 };
 
 buttonS.onclick = function () {
+  element.classList.add("chosen");
   content.innerHTML =
     "In the last decade, smartphones became an absolute necessity for human life. It is the main reason why we build the most innovative apps which help organizations to stay in fashion. Our highly talented and creative team has a capability to develop state-of-the-art mobile applications in various complexity for IOS and Android platforms. We implement advanced features with a delicate user interface and establish a secure data environment.";
-  //   this.style.backgroundColor = " #566cb3";
 };
 
 buttonT.onclick = function () {
+  element.classList.add("chosen");
   content.innerHTML =
     "Today, business world, to a large extent, run with operative software programs. In Caspian Soft, we are specialized in implementing a wide range of software solutions to help our clients to maximize their productivity, efficiency, and profitability. These sophisticated programs do not just provide convenience but also assure the professionalism and competitiveness in a modern business environment.";
-  // changeColor();
 };
 
 var typed = new Typed("#typed", {
